edae33a78797b0c8a771a84265b9eb34de584de5
Troy Kinsella <troy.kinsella@startmail.com>
2017-05-05 11:24:27 -0700

    Colours

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -30,6 +30,33 @@ if test -d $HOME/bin
   set -x PATH $HOME/bin $PATH
 end
 
+# Colours
+
+# override fish colors to use the stock 16 colors, so that it can be themed with .Xresources
+set fish_color_autosuggestion white
+set fish_color_command blue
+set fish_color_comment yellow
+set fish_color_cwd blue
+set fish_color_cwd_root red
+set fish_color_end magenta
+set fish_color_error red
+set fish_color_escape cyan
+set fish_color_history_current cyan
+set fish_color_match cyan
+set fish_color_normal normal
+set fish_color_operator cyan
+set fish_color_param green
+set fish_color_quote cyan
+set fish_color_redirection red
+set fish_color_search_match \x2d\x2dbackground\x3dpurple
+set fish_color_selection \x2d\x2dbackground\x3dpurple
+set fish_color_valid_path \x2d\x2dunderline
+set fish_key_bindings fish_default_key_bindings
+set fish_pager_color_completion normal
+set fish_pager_color_description 555\x1eyellow
+set fish_pager_color_prefix cyan
+set fish_pager_color_progress cyan
+
 # Tmux
 
 alias tmux "tmux -2"
